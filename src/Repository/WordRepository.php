c27bc0905e15ac827292775702a03926f3a9b12f
lucashbarbosa <56563282+lucashbarbosa@users.noreply.github.com>
2021-04-01 01:16:30 -0300

    Update WordRepository.php
    fix words acentuation

--- src/Repository/WordRepository.php
+++ src/Repository/WordRepository.php
@@ -13,13 +13,13 @@ class WordRepository
     {
         return [
             "Abacaxi", "Manada", "mandar", "porta", "mesa", "Dado", "Mangas", "Ja", "coisas",
-            "radiografia", "matemática", "Drogas", "prédios", "implementacao", "computador", "balao",
+            "radiografia", "matematica", "Drogas", "prédios", "implementacao", "computador", "balao",
             "Xicara", "Tedio", "faixa", "Livro", "deixar", "superior", "Profissao", "Reuniao", "Predios",
             "Montanha", "Botanica", "Banheiro", "Caixas", "Xingamento", "Infestacao", "Cupim",
             "Premiada", "empanada", "Ratos", "Ruido", "Antecedente", "Empresa", "Emissario", "Folga",
             "Fratura", "Goiaba", "Gratuito", "Hidrico", "Homem", "Jantar", "Jogos", "Montagem",
             "Manual", "Nuvem", "Neve", "Operacao", "Ontem", "Pato", "Pe", "viagem", "Queijo", "Quarto",
-            "Quintal", "Solto", "rota", "Selva", "Tatuagem", "Tigre", "Uva", "Último", "Vituperio",
+            "Quintal", "Solto", "rota", "Selva", "Tatuagem", "Tigre", "Uva", "ultimo", "Vituperio",
             "Voltagem", "Zangado", "Zombaria", "Dor"
         ];
 
